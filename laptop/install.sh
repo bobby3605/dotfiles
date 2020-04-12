4bcc5b7181d653e64e6f1a3634d545a6fef6dcd9
Bobby <sonic921150@gmail.com>
2020-04-12 14:10:19 -0500

    Adding features

--- laptop/install.sh
+++ laptop/install.sh
@@ -1,22 +1,29 @@
 #!/usr/bin/env bash
 
 #Dependencies
-#sudo pacman -Sy git emacs ripgrep tar fd-find clang xmonad xmonad-contrib xmobar stalonetray xcompmgr rofi
+sudo pacman -Sy xorg-xset xloadimage lightdm-gtk-greeter git emacs ripgrep tar clang xmonad xmonad-contrib xmobar stalonetray xcompmgr rofi termite xorg-server compton
 
 #Emacs
-git clone https://github.com/hlissner/doom-emacs ~/.emacs.d=
+git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
 ~/.emacs.d/bin/doom install
 cp -r .doom.d "$HOME/.doom.d/"
 ~/.emacs.d/bin/doom sync
 
 # XMonad
-#cp -r .xmonad "$HOME/.xmonad"
+cp -r .xmonad "$HOME/.xmonad"
 
 # ZSH STUFF
-#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-#cp .zsh_history "$HOME/.zsh_history"
-#cp .zshrc "$HOME/.zshrc"
-
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cp .zsh_history "$HOME/.zsh_history"
+cp .zshrc "$HOME/.zshrc"
 
 mkdir -p ~/.zsh
 git clone https://github.com/sindresorhus/pure.git .zsh/pure
+
+#Random configs
+cp onedark.rasi ~/onedark.rasi
+cp lightdm.conf /etc/lightdm/lightdm.conf
+mkdir -p ~/.config/termite/
+cp config ~/.config/termite/config
+cp .xsession ~/.xsession
+gpasswd bobby autologin
